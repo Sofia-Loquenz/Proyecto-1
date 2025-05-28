b205f8922fbb10d473498f822231dd94b390e5e7
So <sofialoquenz8@gmail.com>
2025-05-28 20:34:33 +0000

    clase3

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return  render_template("slmain.html")
+    return  render_template("index.html")
+@app.route("/proyectos")
+def proyectos():
+    return render_template("proyectos.html")
     
+if __name__ == '__main__':
+    app.run(debug=True)
